7a048d1f717c0bbcf9995fc03ec213fed728d49f
LeonKattendick <leon.kattendick@gmail.com>
2023-12-03 19:58:31 +0100

    proxy setup

--- packages/doctor/vite.config.ts
+++ packages/doctor/vite.config.ts
@@ -1,10 +1,16 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 8081,
+    proxy: {
+      "/fhir": {
+        target: "http://128.140.65.237",
+        changeOrigin: true,
+      },
+    },
   },
 });
