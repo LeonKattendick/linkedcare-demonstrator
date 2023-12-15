f8b8a822c64bc7b4b2ea3e553d591248b3d8b4dd
LeonKattendick <leon.kattendick@gmail.com>
2023-12-16 00:24:34 +0100

    add button

--- packages/caregiver/src/component/page/ViewPatient/ActiveOrders.tsx
+++ packages/caregiver/src/component/page/ViewPatient/ActiveOrders.tsx
@@ -1,6 +1,8 @@
 import { FileAddOutlined } from "@ant-design/icons";
-import { Button, Card, Space } from "antd";
+import { Button, Card } from "antd";
 import { Patient } from "core/src/interface/linca/Patient";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router";
 import { OrderTable } from "./OrderTable";
 
 interface ActiveOrdersProps {
@@ -8,14 +10,28 @@ interface ActiveOrdersProps {
 }
 
 export const ActiveOrders = ({ patient }: ActiveOrdersProps) => {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+
   return (
     <Card style={{ height: "100%" }}>
-      <Space direction="vertical" style={{ width: "100%" }}>
-        <OrderTable patient={patient} />
-        <Button type="primary" icon={<FileAddOutlined />}>
-          NEU BESTELLUNG
-        </Button>
-      </Space>
+      <OrderTable
+        patient={patient}
+        title={
+          <>
+            {t("viewPatient.activeOrders.title")}
+            <Button
+              type="primary"
+              size="small"
+              icon={<FileAddOutlined />}
+              style={{ float: "right" }}
+              onClick={() => navigate(`/create/${patient.id}`)}
+            >
+              {t("viewPatient.activeOrders.add")}
+            </Button>
+          </>
+        }
+      />
     </Card>
   );
 };
--- packages/caregiver/src/component/page/ViewPatient/OrderTable.tsx
+++ packages/caregiver/src/component/page/ViewPatient/OrderTable.tsx
@@ -3,10 +3,18 @@ import { Patient } from "core/src/interface/linca/Patient";
 
 interface OrderTableProps {
   patient: Patient;
+  title: JSX.Element;
 }
 
-export const OrderTable = ({ patient }: OrderTableProps) => {
-  console.log(patient);
-
-  return <Table />;
+export const OrderTable = (props: OrderTableProps) => {
+  return (
+    <Table
+      dataSource={[{}]}
+      style={{ minHeight: "100%" }}
+      pagination={{ simple: true }}
+      size="small"
+      title={() => props.title}
+      bordered
+    />
+  );
 };
