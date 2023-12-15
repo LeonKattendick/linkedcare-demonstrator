7c65b6c70c8772e9ddee3327d46203373d232da7
LeonKattendick <leon.kattendick@gmail.com>
2023-12-16 00:37:22 +0100

    save

--- packages/caregiver/src/component/page/ViewPatient/PatientTitle.tsx
+++ packages/caregiver/src/component/page/ViewPatient/PatientTitle.tsx
@@ -13,10 +13,16 @@ export const PatientTitle = ({ patient }: PatientTitleProps) => {
   const navigate = useNavigate();
 
   return (
-    <Card>
+    <Card size="small" style={{ paddingLeft: 12 }}>
+      <span style={{ fontSize: 20, fontWeight: "lighter" }}>{t("translation:viewPatient.patientInfo.title")}</span>
       <Flex align="center" gap={16}>
-        <span style={{ fontSize: 20, fontWeight: "bold" }}>{patient.name[0]?.text}</span>
-        <Button type="primary" onClick={() => navigate(`/plan/${patient.id}`)} icon={<MedicineBoxOutlined />}>
+        <span style={{ fontSize: 22, fontWeight: "bold" }}>{patient.name[0]?.text}</span>
+        <Button
+          type="primary"
+          onClick={() => navigate(`/plan/${patient.id}`)}
+          icon={<MedicineBoxOutlined />}
+          size="small"
+        >
           {t("viewPatient.medicationPlan")}
         </Button>
       </Flex>
