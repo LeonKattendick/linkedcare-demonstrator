1aa72b549da47367271bc68ea97ebf0e141be086
LeonKattendick <leon.kattendick@gmail.com>
2023-12-27 02:53:31 +0100

    new doctor

--- packages/core/src/model/doctorModels.ts
+++ packages/core/src/model/doctorModels.ts
@@ -28,4 +28,18 @@ const wuerm: Practitioner = {
   address: [],
 };
 
-export const doctorModels = [spitzmaus, wuerm];
+const otter: Practitioner = {
+  resourceType: "Practitioner",
+  active: true,
+  identifier: [
+    {
+      system: "urn:oid:1.2.40.0.34.5.2",
+      value: "2.999.40.0.34.3.1.2",
+    },
+  ],
+  name: [{ use: "official", text: "Dr. Kunibert Kreuzotter", given: ["Kunibert"], family: "Kreuzotter" }],
+  telecom: [],
+  address: [],
+};
+
+export const doctorModels = [spitzmaus, wuerm, otter];
