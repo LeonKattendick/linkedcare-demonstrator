5e7ea2b9075eafb1cbb6528d2457633bdd753efb
LeonKattendick <leon.kattendick@gmail.com>
2023-12-29 01:38:35 +0100

    doctor order and med plan

--- packages/doctor/src/component/page/DoctorMedicationPlan.tsx
+++ packages/doctor/src/component/page/DoctorMedicationPlan.tsx
@@ -1,3 +1,16 @@
+import { PatientNotFoundError } from "core/src/component/Error/PatientNotFoundError";
+import { Loading } from "core/src/component/Loading";
+import { MedicationPlan } from "core/src/component/page/MedicationPlan";
+import { useGetPatientById } from "core/src/hook/useGetPatientById";
+import { useParams } from "react-router";
+
 export const DoctorMedicationPlan = () => {
-  return <></>;
+  const { patientId } = useParams();
+
+  const { patient, isPatientLoading } = useGetPatientById(patientId);
+
+  if (isPatientLoading) return <Loading />;
+  if (!patient) return <PatientNotFoundError patientId={patientId} />;
+
+  return <MedicationPlan patient={patient} />;
 };
--- packages/doctor/src/component/page/DoctorOrder.tsx
+++ packages/doctor/src/component/page/DoctorOrder.tsx
@@ -1,3 +1,30 @@
+import { OrderNotFoundError } from "core/src/component/Error/OrderNotFoundError";
+import { PatientNotFoundError } from "core/src/component/Error/PatientNotFoundError";
+import { Loading } from "core/src/component/Loading";
+import { Order } from "core/src/component/page/Order";
+import { useGetAllMedicationRequestsForOrchestration } from "core/src/hook/useGetAllMedicationRequestsForOrchestration";
+import { useGetPatientById } from "core/src/hook/useGetPatientById";
+import { useGetRequestOrchestrationById } from "core/src/hook/useGetRequestOrchestrationById";
+import { identifierEqualsReference } from "core/src/util/matchingUtil";
+import { Navigate, useParams } from "react-router";
+import { useSelectedDoctorAtom } from "../../hook/useSelectedDoctorAtom";
+
 export const DoctorOrder = () => {
-  return <></>;
+  const { patientId, orderId } = useParams();
+
+  const { selectedDoctor } = useSelectedDoctorAtom();
+  const { patient, isPatientLoading } = useGetPatientById(patientId);
+
+  const { orchestration, isOrchestrationLoading } = useGetRequestOrchestrationById(orderId);
+  const { requests, isRequestsLoading } = useGetAllMedicationRequestsForOrchestration(orderId, patientId);
+
+  if (isPatientLoading || isOrchestrationLoading || isRequestsLoading) return <Loading />;
+  if (!patient) return <PatientNotFoundError patientId={patientId} />;
+
+  if (!orchestration) return <OrderNotFoundError orderId={orderId} />;
+  if (!requests.find((v) => identifierEqualsReference(selectedDoctor?.identifier[0], v.performer[0]))) {
+    return <Navigate to="/" />;
+  }
+
+  return <Order patient={patient} order={orchestration} doctor={selectedDoctor!} />;
 };
