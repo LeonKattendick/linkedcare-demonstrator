1d1e2dcb82e162faf045c8a37e1f6aa96f7bcc99
LeonKattendick <leon.kattendick@gmail.com>
2023-12-29 02:46:52 +0100

    filter linked requests

--- packages/core/src/hook/useGetAllMedicationRequestsForOrchestration.ts
+++ packages/core/src/hook/useGetAllMedicationRequestsForOrchestration.ts
@@ -1,4 +1,6 @@
 import { useMemo } from "react";
+import { PrescriptionMedicationRequest } from "../interface/linca/PrescriptionMedicationRequest";
+import { InternalReference } from "../interface/linca/fhir/Reference";
 import { requestIsFromOrchestration } from "../util/matchingUtil";
 import { useGetAllMedicationRequestsByPatient } from "./useGetAllMedicationRequestsByPatient";
 import { useGetAllRequestOrchestrations } from "./useGetAllRequestOrchestrations";
@@ -13,7 +15,18 @@ export const useGetAllMedicationRequestsForOrchestration = (
   // Memoization is used to not compute this value on every rerender of the component
   const relevantRequests = useMemo(() => {
     const orchestration = orchestrations.find((v) => v.id === orderId);
-    return requests.filter((v) => !!requestIsFromOrchestration(v, orchestration));
+    const requestsForOrchestration = requests.filter((v) => !!requestIsFromOrchestration(v, orchestration));
+
+    const linkedRequestIds: string[] = [];
+    for (const request of requestsForOrchestration) {
+      const basedOn = request.basedOn as InternalReference;
+      const priorPrescription = (request as PrescriptionMedicationRequest).priorPrescription as InternalReference;
+
+      if (!!basedOn) linkedRequestIds.push(basedOn.reference!);
+      if (!!priorPrescription) linkedRequestIds.push(priorPrescription.reference!);
+    }
+
+    return requestsForOrchestration.filter((v) => !linkedRequestIds.includes(`MedicationRequest/${v.id}`));
   }, [orchestrations, requests]);
 
   return { requests: relevantRequests ?? [], isRequestsLoading: isOrchestrationsLoading || isRequestsLoading };
